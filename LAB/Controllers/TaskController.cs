af4b417ef2ef854a604feff6ec73cd13ea1aad33
JakubW99 <jakubwojcikiewicz1@gmail.com>
2022-11-09 12:39:39 +0100

    Dodaj pliki projektów.

---
+++ LAB/Controllers/TaskController.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using LAB.Models;
+namespace LAB.Controllers
+{
+    public class TaskController : Controller
+    {
+        private static List<Models.Task> tasks = new List<Models.Task>()
+        {
+            new Models.Task()
+            {
+                Id =1, Email = "jan@wp.pl", Author= "Janek", TaskName = "Zakupy", Description =" Zrób zakupy"
+            },
+            new Models.Task()
+            {
+                Id =2, Email = "nowak@gmail.com", Author= "Adam", TaskName = "Jedź do banku", Description ="Zrób przelew"
+            },
+            new Models.Task()
+            {
+                Id =3, Email = "pawel@outlook.com", Author= "Pablo", TaskName = "Uczelnia", Description ="Ucz się"
+            }
+        };
+        int counter = tasks.Count;
+        public IActionResult Index()
+        {
+
+            return View(tasks);
+        }
+        public IActionResult Delete(int id)
+        {
+
+            tasks.RemoveAt(id);
+            counter--;
+            return View("Index", tasks);
+        }
+        public IActionResult Edit(int id)
+        {
+            return View();
+        }
+
+      
+        public IActionResult Form()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ViewResult Form(LAB.Models.Task task)
+        {
+
+            if (ModelState.IsValid)
+            {
+                tasks.Add(task);
+                return View("Index",tasks);
+            }
+            else
+            {
+                return View();
+                //ponowne wy
+
+            }
+
+        }
+    }
+}
---
+++ LAB/Models/Task.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
+
+namespace LAB.Models
+{
+    public class Task
+    {
+        [HiddenInput]
+        public int Id { get; set; }
+        [Required(ErrorMessage = "Podaj Autora")]
+        public string Author { get; set; }
+        [RegularExpression(".+\\@.+\\.[a-z]{2,3}")]
+        [Required(ErrorMessage = "Podaj poprawny Email")]
+        public string Email { get; set; }
+        [Required(ErrorMessage = "Podaj nazwe zadania")]
+        public string TaskName { get; set; }
+        [MinLength(length: 5,ErrorMessage ="Opis musi mieć conajmniej 5 znaków")]
+        public string Description { get; set; }
+
+    }
+}
